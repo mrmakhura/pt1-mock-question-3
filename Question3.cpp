3f9d10643efd95907d75e796b66291331fcc707f
mrmakhura <Stonewall305>
2015-04-20 06:43:13 +0200

    final question 3

--- Question3.cpp
+++ Question3.cpp
@@ -5,6 +5,8 @@
 // Date: 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
+#include<string>
+
 
 using namespace std;
 
@@ -22,6 +24,75 @@ private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
-	void print() { cout << num << "/" << denom; };
+	Fraction(int , int ) ;
+	void print();
+	friend bool operator>(Fraction &,Fraction &);
+	void add(Fraction&, int);
+	void add(int, Fraction&);
+
 };
+
+bool operator>(Fraction &x, Fraction &y)
+{
+	int xx, yy;
+	xx = x.num / x.denom;
+	yy = y.num / y.denom;
+
+	return(xx > yy);
+}
+
+void Fraction::add(Fraction &v, int x)
+{
+	v.num = v.num+ v.denom*x;
+	v.print();
+cout << endl;
+}
+
+void Fraction::add(int x, Fraction &v)
+{
+	v.num = v.num + v.denom*x;
+	v.print();
+	cout << endl;
+}
+	
+void Fraction::print()
+{
+	cout << num << "/" << denom;
+}
+
+Fraction::Fraction(int n,int d): num(n), denom(d)
+{
+
+}
+
+int main()
+{  
+	
+	Fraction frac1(2, 3);
+	Fraction frac2(1, 2);
+	
+	if (frac1 > frac2)
+	{
+		frac1.print();
+		cout << " is greater than ";
+		frac2.print();
+	}
+	else
+	{
+		frac2.print();
+		cout << " is greater than ";
+		frac1.print();
+		cout << endl << endl;
+	}
+	
+	frac1.add(2,frac1);
+	frac1.add(frac1, 3);
+	frac2.add(frac2, 2);
+	frac2.add(2, frac2);
+
+
+
+
+	return 0;
+
+}
